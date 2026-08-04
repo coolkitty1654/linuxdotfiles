9648fd8ffbf831ffa35f08d1bab0fa83c2985779
coolkitty1654 <96185065+coolkitty1654@users.noreply.github.com>
2026-08-04 16:46:54 +0800

    update to lua

---
+++ OneShot/TWM/hypr/hyprland.lua
@@ -0,0 +1,23 @@
+
+--  _   _                  _                 _  
+-- | | | |_   _ _ __  _ __| | __ _ _ __   __| | 
+-- | |_| | | | | '_ \| '__| |/ _` | '_ \ / _` | 
+-- |  _  | |_| | |_) | |  | | (_| | | | | (_| | 
+-- |_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_| 
+--        |___/|_|                              
+--  
+-- ----------------------------------------------------- 
+-- Full documentation https://wiki.hyprland.org
+
+require("config/monitor")
+require("config/autostart")
+require("config/cursor")
+require("config/environments")
+require("config/input")
+require("config/general")
+require("config/decoration")
+require("config/animations")
+require("config/layout")
+require("config/gestures")
+require("config/misc")
+require("config/binds")
